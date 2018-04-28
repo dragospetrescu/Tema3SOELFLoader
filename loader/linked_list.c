f277fd4ae11d250befd1b89b2305358ca2be43e8
dragos <petrescudragos1995@gmail.com>
2018-04-29 00:56:06 +0300

    add linked list c

--- loader/linked_list.c
+++ loader/linked_list.c
@@ -3,3 +3,66 @@
 //
 
 #include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+struct Node
+{
+	uintptr_t data;
+	struct Node *next;
+};
+
+
+void append(struct Node** head_ref, uintptr_t new_data)
+{
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+	struct Node *last = *head_ref;
+
+	new_node->data  = new_data;
+
+	new_node->next = NULL;
+
+	if (*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		return;
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	return;
+}
+
+void printList(struct Node *node)
+{
+	while (node != NULL)
+	{
+		printf(" %d ", node->data);
+		node = node->next;
+	}
+}
+
+int contain(struct Node** head_ref, uintptr_t data)
+{
+	struct Node *last = *head_ref;
+	if (*head_ref == NULL)
+	{
+		printf("List is empty \n");
+	}
+	else
+	{
+		do
+		{
+			if(data==last->data)
+				return 1;
+			last = last->next;
+		}while (last->next != NULL);
+	}
+
+	return 0;
+}
