601fc8a7f971d8e5c344053e0c55fec05fabb9db
dragos <petrescudragos1995@gmail.com>
2018-05-02 01:50:37 +0300

    copying stuff. nothing works

--- loader/loader.c
+++ loader/loader.c
@@ -16,26 +16,31 @@
 #include "linked_list.h"
 #include "exec_parser.h"
 
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
 static so_exec_t *exec;
-static char * executable_file;
+static void *executable_file;
 static struct sigaction old_action;
 
-static so_seg_t *get_segment(void *page_start_pointer)
+static int get_segment(void *page_start_pointer)
 {
 	uintptr_t page_start = (uintptr_t) page_start_pointer;
+	fprintf(stderr, "Looking for %p int %d segments\n", page_start_pointer, exec->segments_no);
 	for (int i = 0; i < exec->segments_no; i++) {
-		fprintf(stderr, "Segment %i: %p mem_size: %d file_size: %d\n",
-				i,
-				(void *) exec->segments[i].vaddr,
-				(int) exec->segments[i].mem_size,
-				(int) exec->segments[i].file_size);
+
 		if (exec->segments[i].vaddr <= page_start
 			&& exec->segments[i].vaddr + exec->segments[i].mem_size
 				>= page_start) {
-			return &exec->segments[i];
+			fprintf(stderr, "Segment %i: %p mem_size: %d file_size: %d\n",
+					i,
+					(void *) exec->segments[i].vaddr,
+					(int) exec->segments[i].mem_size,
+					(int) exec->segments[i].file_size);
+			return i;
 		}
 	}
-	return NULL;
+//	fprintf(stderr, "SEGMENT NOT FOUND\n");
+	return -1;
 }
 
 static void segv_handler(int signum, siginfo_t *info, void *context)
@@ -48,18 +53,22 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 
 	addr = info->si_addr;
 	if (addr == NULL) {
+		fprintf(stderr, "MERE\n");
 		old_action.sa_sigaction(signum, info, context);
 		return;
 	}
 	int pageSize = getpagesize();
 	void *page_start = (void *) ((int) addr & ~(pageSize - 1));
 
-	so_seg_t *segment = get_segment(page_start);
+	int seg_no = get_segment(page_start);
 
-	if (segment == NULL) {
+	if (seg_no == -1) {
 		old_action.sa_sigaction(signum, info, context);
 		return;
 	}
+
+	struct so_seg *segment = &exec->segments[seg_no];
+
 
 	LINKED_LIST *segment_used_pages = (LINKED_LIST *) segment->data;
 	if (!contains(&segment_used_pages, (uintptr_t) page_start)) {
@@ -71,9 +80,12 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 		return;
 	}
 
+	int page_no = ((int)segment->vaddr - (int )page_start) / pageSize;
+//	fprintf(stderr, " %d\n" , page_no);
+
 	void *result = mmap(page_start,
 						pageSize,
-						segment->perm,
+						PROT_WRITE | PROT_READ | PROT_EXEC,
 						MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED,
 						-1,
 						0);
@@ -81,20 +93,36 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 		perror("MMAP FAILED");
 		exit(0);
 	}
-	else {
-		fprintf(stderr, "Asked for %p received %p\n", page_start, result);
+//	else {
+//		fprintf(stderr, "Asked for %p received %p\n", page_start, result);
+//	}
+	size_t lungime_date;
+	size_t lungime_zero;
+	if((uintptr_t)page_start < segment->vaddr + segment->file_size) {
+		lungime_date = MIN(pageSize, -(int)page_start + segment->vaddr + segment->file_size);
+
+		memcpy(result, executable_file + segment->offset + pageSize * page_no, lungime_date);
+		fprintf(stderr, "INFO at: %p %p %d\n", result, executable_file, lungime_date);
 	}
 
-//	if((uintptr_t)page_start >= segment->vaddr &&
-//		(uintptr_t)page_start + pageSize < segment->vaddr + segment->file_size) {
+	if((uintptr_t)page_start + pageSize > segment->vaddr + segment->file_size) {
+		lungime_zero = pageSize + (int)page_start - segment->vaddr - segment->file_size;
+		if (lungime_zero > 0)
+			memset(result + lungime_date, '\0', lungime_zero);
+		fprintf(stderr, "ZERO at: %p %d\n", result + lungime_date, lungime_zero);
+	}
+
+//	if((uintptr_t)page_start + pageSize < segment->vaddr + segment->file_size) {
 //		// TODO: copy pagesize bytes from file
-//	} else if ((uintptr_t)page_start >= segment->vaddr &&
-//		(uintptr_t)page_start + pageSize > segment->vaddr + segment->file_size &&
+//		fprintf(stderr, "CAZ 1\n");
+//	} else if ((uintptr_t)page_start + pageSize > segment->vaddr + segment->file_size &&
 //		(uintptr_t)page_start + pageSize < segment->vaddr + segment->mem_size
 //		) {
-//
-//
-//	} else if ((uintptr_t)page_start >= segment->vaddr + segment->file_size &&
+//		// TODO: copy pagesize bytes from file
+//		fprintf(stderr, "CAZ 2 - copiez si zeroizez\n");
+//	}
+
+// else if ((uintptr_t)page_start >= segment->vaddr + segment->file_size &&
 //		(uintptr_t)page_start + pageSize < segment->vaddr + segment->mem_size) {
 //
 //	} else {
