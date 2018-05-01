8e594f9ab858c8e230543a3bb58af7430a0da41d
dragos <petrescudragos1995@gmail.com>
2018-05-01 13:47:43 +0300

    test_remote.sh change to add linked list

--- test_remote.sh
+++ test_remote.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-scp loader/loader.c student@172.16.245.130:/home/student/Tema3/loader
+scp loader/l* student@172.16.245.130:/home/student/Tema3/loader
 ssh student@172.16.245.130 /home/student/Tema3/run.sh
